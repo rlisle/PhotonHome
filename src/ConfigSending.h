a3076817e9f388202ca3ecbf02015a8e79cc17be
Ron Lisle <ron.lisle@mutualmobile.com>
2019-05-24 07:26:57 -0500

    Add ConfigSending protocol

---
+++ src/ConfigSending.h
@@ -0,0 +1,9 @@
+//
+// This class defines the Protocol used to request sending 
+// Home Assistant automatic discovery config messages.
+//
+class ConfigSending
+{
+public:
+    virtual void sendConfig() = 0;
+};
